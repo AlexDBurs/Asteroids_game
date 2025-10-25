4ee628738486df5a33d8806d91dcf0b4c17ada35
Alex <alexd.burs@gmail.com>
2025-10-25 21:19:10 +0000

    Added sprites

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,17 +12,27 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
-        
+
         #Event Loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        
+        
         # Screen rendering
         screen.fill("black")
+
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
